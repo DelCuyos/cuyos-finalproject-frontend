f3dde79538e4c5efab841e07ab18e4cbdcb206ae
delcuyos <DelCuyos>
2026-05-22 07:46:19 +0800

    fix

--- src/app/profile/details.component.spec.ts
+++ src/app/profile/details.component.spec.ts
@@ -1,5 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsComponent } from './details.component';
 
