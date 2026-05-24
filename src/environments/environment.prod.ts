b985aaa5799c05ac2ca2faa917dfaab7ed09c2bb
delcuyos <DelCuyos>
2026-05-24 16:51:44 +0800

    fix backend API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://cuyos-finalproject-backend.onrender.com'
+    apiUrl: 'https://cuyos-finalproject-backend-mrrp.onrender.com'
 };
 
 
